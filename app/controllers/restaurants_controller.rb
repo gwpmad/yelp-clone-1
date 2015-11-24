fdc1a99c4bc2fabae551987b054864bcac93ce39
George Maddocks <georgemaddocks@yahoo.co.uk>
2015-11-24 12:05:01 +0000

    adding restaurants - form and route

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,18 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def restaurant_params
+    #only allow the field labelled 'name' to be accepted by the form
+    params.require(:restaurant).permit(:name)
+  end
 end
