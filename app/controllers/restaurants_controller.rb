41ec6ec89a62d3409543e6b93bc30ddab313baca
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2015-11-24 14:50:51 +0000

    able to edit restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -16,6 +16,16 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
   def restaurant_params
     #only allow the field labelled 'name' to be accepted by the form
     params.require(:restaurant).permit(:name)
